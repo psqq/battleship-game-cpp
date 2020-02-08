7f52fe7203410858d1b22a25862209427396d23f
Sergey Pestov <pestov-sa@yandex.ru>
2020-02-08 11:02:22 +0300

    ttf works

--- src/sdl2/main.cpp
+++ src/sdl2/main.cpp
@@ -1,11 +1,11 @@
 #include <SDL.h>
+#include <SDL_ttf.h>
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
-
     SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
 
     SDL_Window* window = SDL_CreateWindow(
         "Battleship game",
@@ -18,8 +18,22 @@ int main(int argc, char* argv[])
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+
+    TTF_Font* Sans = TTF_OpenFont("assets/Open_Sans/OpenSans-Regular.ttf", 24);
+
+    if (!Sans) {
+        std::cout << "No font!" << std::endl;
+    }
+
+    SDL_Color White = { 255, 255, 255 };
+    SDL_Surface* surfaceMessage = TTF_RenderText_Solid(Sans, "put your text here", White);
+    SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+
+    SDL_Rect Message_rect;
+    Message_rect.x = 0;
+    Message_rect.y = 0;
+    Message_rect.w = 200;
+    Message_rect.h = 100;
 
     while (true)
     {
@@ -33,6 +47,9 @@ int main(int argc, char* argv[])
                 break;
             }
         }
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
+        SDL_RenderPresent(renderer);
     }
 
     SDL_DestroyWindow(window);
