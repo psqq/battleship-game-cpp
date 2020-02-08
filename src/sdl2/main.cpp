f48279940caf6be3cb5f03a4b4d8362b03f209f4
Sergey Pestov <pestov-sa@yandex.ru>
2020-02-08 10:15:51 +0300

    linking sdl2 version works

--- src/sdl2/main.cpp
+++ src/sdl2/main.cpp
@@ -1,11 +1,14 @@
 #include <SDL.h>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
+    std::cout << "Hello, World!" << std::endl;
+
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window* window = SDL_CreateWindow(
-        "SDL2Test",
+        "Battleship game",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         640,
@@ -18,7 +21,19 @@ int main(int argc, char* argv[])
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
-    SDL_Delay(3000);
+    while (true)
+    {
+        // Get the next event
+        SDL_Event event;
+        if (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                // Break out of the loop on quit
+                break;
+            }
+        }
+    }
 
     SDL_DestroyWindow(window);
     SDL_Quit();
